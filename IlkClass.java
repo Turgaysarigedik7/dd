34c788e3c9e36f27e6a804cc6cbcf900bf8df361
Turgaysarigedik7 <turgaysarigedik@gmail.com>
2023-02-28 14:08:15 +0300

    d1

---
+++ IlkClass.java
@@ -0,0 +1,43 @@
+public class IlkClass {
+    public static void main(String[] args) {
+        String ilkString = "hello world!";
+
+        boolean ogrendikMi = true;
+
+        char ilkChar = 'X';
+
+        int sayi = 14;
+
+        double ondalik = 36.44;
+
+        //*******************************
+
+        String isim;
+        char karakter;
+        boolean durum;
+        int yas;
+        double ondalikliSayi;
+
+        isim = "Turgay";
+        karakter = 't';
+        durum = true;
+        yas=23;
+        ondalikliSayi = 34.5;
+
+
+        System.out.println(isim);
+        System.out.println(karakter);
+        System.out.println(durum);
+        System.out.println(yas);
+        System.out.println(ondalikliSayi);
+
+        //**********************
+
+        String name ="turgay", surName="sarigedik";
+        char karakter1='t',karakter2= 's';
+        boolean durum1=true, durum2=false;
+        int boy=176,kilo=70;
+        double sayi1=3.5,sayi2=7.6;
+    }
+}
+
